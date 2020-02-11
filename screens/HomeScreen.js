74cfb0a9e63648d39686608e9ec342b935a8c685
Pawel Bala <pawel3ala@gmail.com>
2020-02-10 21:52:16 -0600

    sending out aztec to server

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,6 +7,7 @@ import {
   StatusBar
 } from 'react-native'
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import axios from 'axios'
 
 export default function HomeScreen() {
   const [hasPermission, setHasPermission] = useState(null);
@@ -22,6 +23,14 @@ export default function HomeScreen() {
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+
+    let formData = new FormData()
+    formData.append("action", "datatransfer")
+    formData.append("terminal", "99999999YYYYYYYY")
+    formData.append("barcode", Base64.btoa(data))
+
+    axios.post("http://kup.direct/appconnect/service.php", formData)
+      .then((resp) => console.log(getSessionId(resp.request._response)))
   };
 
   if (hasPermission === null) {
@@ -95,3 +104,52 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 })
+
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+const Base64 = {
+  btoa: (input)  => {
+    let str = input;
+    let output = '';
+
+    for (let block = 0, charCode, i = 0, map = chars;
+    str.charAt(i | 0) || (map = '=', i % 1);
+    output += map.charAt(63 & block >> 8 - i % 1 * 8)) {
+
+      charCode = str.charCodeAt(i += 3/4);
+
+      if (charCode > 0xFF) {
+        throw new Error("'btoa' failed: The string to be encoded contains characters outside of the Latin1 range.");
+      }
+      
+      block = block << 8 | charCode;
+    }
+    
+    return output;
+  },
+
+  atob: (input) => {
+    let str = input.replace(/=+$/, '');
+    let output = '';
+
+    if (str.length % 4 == 1) {
+      throw new Error("'atob' failed: The string to be decoded is not correctly encoded.");
+    }
+    for (let bc = 0, bs = 0, buffer, i = 0;
+      buffer = str.charAt(i++);
+
+      ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
+        bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
+    ) {
+      buffer = chars.indexOf(buffer);
+    }
+
+    return output;
+  }
+};
+
+
+const getSessionId = (string) => {
+  const indexOfEqualSign = string.lastIndexOf('=') + 1
+  const indexOfLastQuotes = string.lastIndexOf('"')
+  return string.slice(indexOfEqualSign, indexOfLastQuotes)
+}
