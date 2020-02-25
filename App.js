a4066b5c680f1378113b8f382f3f534f3a34c66e
Pawel Bala <pawel3ala@gmail.com>
2020-02-25 17:09:09 -0600

    adding Offers screen

--- App.js
+++ App.js
@@ -1,26 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  StatusBar,
-  Platform
-} from 'react-native'
-import { BarCodeScanner } from 'expo-barcode-scanner';
-import axios from 'axios'
-import { WebView } from 'react-native-webview';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { ScreenOrientation } from 'expo';
-import base64 from 'react-native-base64'
-import * as Sentry from 'sentry-expo';
 import ScannerScreen from "./screens/ScannerScreen"
-
-Sentry.init({
-  dsn: 'https://f8a02133e800455c86bee49793874e17@sentry.io/2581571',
-  enableInExpoDevelopment: true,
-  debug: true
-});
+import OffersScreen from "./screens/OffersScreen"
 
 const Stack = createStackNavigator();
 
@@ -30,35 +12,8 @@ export default function App() {
       <Stack.Navigator
         screenOptions={{ headerShown: false }}>
         <Stack.Screen name="ScannerScreen" component={ScannerScreen} />
-        <Stack.Screen name="Oferta" component={Oferta} />
+        <Stack.Screen name="OffersScreen" component={OffersScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-function Oferta(props) {
-
-  useEffect(() => {
-    (async () => {
-      await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
-    })();
-  }, []);
-
-  return (
-    <>
-      <StatusBar hidden={true} />
-      <WebView
-        // scalesPageToFit
-        // javaScriptEnabled
-        // style={{ flex: 1 }}
-        onError={syntheticEvent => {
-          const { nativeEvent } = syntheticEvent;
-          Sentry.captureMessage('WebView error: ' + nativeEvent, 'error');
-        }}
-        source={{ uri: `http://kup.direct/appconnect/service.php?session=${props.route.params.sessionId}` }}
-      />
-    </>
-  );
-}
-
-// http://kup.direct/appconnect/service.php?session=1d1fd1457dee2864fb36c76d6fdbc2e5769f0f2f
---
+++ screens/OffersScreen.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { StatusBar } from 'react-native'
+import { WebView } from 'react-native-webview';
+import { ScreenOrientation } from 'expo';
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+    dsn: 'https://f8a02133e800455c86bee49793874e17@sentry.io/2581571',
+    enableInExpoDevelopment: true,
+    debug: true
+});
+
+export default function Offers(props) {
+
+    useEffect(() => {
+        (async () => {
+            await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+        })();
+    }, []);
+
+    return (
+        <>
+            <StatusBar hidden={true} />
+            <WebView
+                // scalesPageToFit
+                // javaScriptEnabled
+                // style={{ flex: 1 }}
+                onError={syntheticEvent => {
+                    const { nativeEvent } = syntheticEvent;
+                    Sentry.captureMessage('WebView error: ' + nativeEvent, 'error');
+                }}
+                source={{ uri: `http://kup.direct/appconnect/service.php?session=${props.route.params.sessionId}` }}
+            />
+        </>
+    );
+}
